437415fb11f841d5518d4d1fcced118a44bcee81
OseiEsther <oseiamoakoesther2022@gmail.com>
2025-08-04 21:12:15 +0100

    Create grede calculator

--- 11352966_ass1/Program.cs
+++ 11352966_ass1/Program.cs
@@ -1,8 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 
-namespace Calculator
+class Program
 {
 
+
+    static void GradeCalculator()
+    {
+        Console.Write("Enter your numerical grade (0-100): ");
+        if (int.TryParse(Console.ReadLine(), out int grade))
+        {
+            if (grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Grade must be between 0 and 100.");
+                return;
+            }
+
+            string letterGrade = grade >= 90 ? "A" :
+                                 grade >= 80 ? "B" :
+                                 grade >= 70 ? "C" :
+                                 grade >= 60 ? "D" : "F";
+
+            Console.WriteLine($"Your letter grade is: {letterGrade}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+
 }
-Console.WriteLine("hello esther");
--- 11352966_ass1/obj/Debug/net8.0/11352966_ass1.AssemblyInfo.cs
+++ 11352966_ass1/obj/Debug/net8.0/11352966_ass1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("11352966_ass1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c47409e3c9f9b3b0bcaf3c5fd43ebf913eecaca")]
 [assembly: System.Reflection.AssemblyProductAttribute("11352966_ass1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("11352966_ass1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
